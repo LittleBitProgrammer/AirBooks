045fac700d859510bad46e2dfd4b6e619bdd81b7
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2018-02-01 10:47:49 +0100

    FolioReader implemented
    -aggiunta classe HighLightData per evidenziare le parole
    -aggiunto fragment TestFragment per la lettura delle informazioni del libro
    -fix delle dependencies
    -aggiunti alcuni metodi al main per la lettura epub
    -commentate alcune porzioni di codice per riutilizzare il codice della lettura epub in futuro

--- app/src/main/java/it/corelab/airbooks/MainActivity.java
+++ app/src/main/java/it/corelab/airbooks/MainActivity.java
@@ -2,12 +2,120 @@ package it.corelab.airbooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.folioreader.model.HighLight;
+import com.folioreader.ui.base.OnSaveHighlight;
+import com.folioreader.util.FolioReader;
+import com.folioreader.util.OnHighlightListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnHighlightListener {
+
+
+    //=============================
+    //  class variable declaration
+    //=============================
+
+    private FolioReader folioReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // instanza di folioReader
+        //uncomment per lettura epub
+
+        /*
+        FolioReader folioReader = new FolioReader(this);
+        folioReader.registerHighlightListener(this);
+        folioReader.openBook(R.raw.canzone);
+        getHighlightsAndSave();
+        */
+    }
+
+
+    //=============================
+    //            methods
+    //=============================
+
+
+    //uncomment solo quando è possibile leggere epub
+   /* private void getHighlightsAndSave() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<HighLight> highlightList = null;
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    highlightList = objectMapper.readValue(
+                            loadAssetTextAsString("highlights/highlights_data.json"),
+                            new TypeReference<List<HighlightData>>() {
+                            });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (highlightList == null) {
+                    folioReader.saveReceivedHighLights(highlightList, new OnSaveHighlight() {
+                        @Override
+                        public void onFinished() {
+                            //You can do anything on successful saving highlight list
+                        }
+                    });
+                }
+            }
+        }).start();
+    }*/
+    private String loadAssetTextAsString(String name) {
+        BufferedReader in = null;
+        try {
+            StringBuilder buf = new StringBuilder();
+            InputStream is = getAssets().open(name);
+            in = new BufferedReader(new InputStreamReader(is));
+
+            String str;
+            boolean isFirst = true;
+            while ((str = in.readLine()) != null) {
+                if (isFirst)
+                    isFirst = false;
+                else
+                    buf.append('\n');
+                buf.append(str);
+            }
+            return buf.toString();
+        } catch (IOException e) {
+            Log.e("HomeActivity", "Error opening asset " + name);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    Log.e("HomeActivity", "Error closing asset " + name);
+                }
+            }
+        }
+        return null;
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        folioReader.unregisterHighlightListener();
+    }
+    @Override
+    public void onHighlight(HighLight highlight, HighLight.HighLightAction type) {
+        Toast.makeText(this,
+                "highlight id = " + highlight.getUUID() + " type = " + type,
+                Toast.LENGTH_SHORT).show();
     }
 }
