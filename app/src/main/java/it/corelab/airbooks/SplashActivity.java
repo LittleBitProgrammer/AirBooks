9fab28e832bbcf7cb24802ea8e0b2c78690d7a4c
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2018-02-02 16:33:22 +0100

    launchScreen
    -added LaunchScreen

---
+++ app/src/main/java/it/corelab/airbooks/SplashActivity.java
@@ -0,0 +1,15 @@
+package it.corelab.airbooks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
