6c0e7439ffe8f5def061738ec5484be15d384dbc
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2018-02-03 18:43:51 +0100

    Profilo
    -metà alta del profilo completata

--- app/src/main/java/it/corelab/airbooks/mFragments/ProfileFragment.java
+++ app/src/main/java/it/corelab/airbooks/mFragments/ProfileFragment.java
@@ -1,12 +1,11 @@
 package it.corelab.airbooks.mFragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import it.corelab.airbooks.R;
 
@@ -15,9 +14,13 @@ import it.corelab.airbooks.R;
  */
 
 public class ProfileFragment extends android.support.v4.app.Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIstance){
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
         return rootView;
     }
