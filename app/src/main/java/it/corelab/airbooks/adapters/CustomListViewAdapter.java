ff2b26de16402bb30c0b91ffe430110837b2ff44
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2018-03-12 17:19:45 +0100

    All reviews part 2
    - l'activity di all revinew adesso è completamente funzionale e senza bug 
    -aggiunta classe per espandere il testo e le celle 
    - modificata animazione di espansione e chiusura delle celle

--- app/src/main/java/it/corelab/airbooks/adapters/CustomListViewAdapter.java
+++ app/src/main/java/it/corelab/airbooks/adapters/CustomListViewAdapter.java
@@ -1,26 +1,23 @@
 package it.corelab.airbooks.adapters;
 
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
-import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ForegroundColorSpan;
+import android.graphics.Rect;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.TouchDelegate;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
 import java.util.List;
 
-import it.corelab.airbooks.MySpannable;
+import it.corelab.airbooks.widget.ExpandableTextView;
 import it.corelab.airbooks.R;
 import it.corelab.airbooks.object.Review;
 
@@ -29,7 +26,6 @@ import it.corelab.airbooks.object.Review;
  */
 
 public class CustomListViewAdapter extends ArrayAdapter<Review> {
-    public static final int MAX_LINES = 3;
 
     public CustomListViewAdapter(Context context, int textViewResourceId, List<Review> objects) {
         super(context, textViewResourceId, objects);
@@ -43,7 +39,7 @@ public class CustomListViewAdapter extends ArrayAdapter<Review> {
 
         ImageView image = (ImageView) convertView.findViewById(R.id.image_profile_all_reviews);
         TextView textView = (TextView) convertView.findViewById(R.id.name_surname);
-        final TextView description = (TextView) convertView.findViewById(R.id.expandable_text);
+        final ExpandableTextView description = (ExpandableTextView) convertView.findViewById(R.id.expandable_text);
 
         ImageView stella1 = (ImageView) convertView.findViewById(R.id.stella_1);
         ImageView stella2 = (ImageView) convertView.findViewById(R.id.stella_2);
@@ -51,11 +47,41 @@ public class CustomListViewAdapter extends ArrayAdapter<Review> {
         ImageView stella4 = (ImageView) convertView.findViewById(R.id.stella_4);
         ImageView stella5 = (ImageView) convertView.findViewById(R.id.stella_5);
 
+        final Button buttonToggle = (Button) convertView.findViewById(R.id.read_all_button);
+
+        final View parentReturn = (View) buttonToggle.getParent();  // button: the view you want to enlarge hit area
+
+        parentReturn.post( new Runnable() {
+                               public void run() {
+                                   final Rect rect = new Rect();
+                                   buttonToggle.getHitRect(rect);
+                                   rect.top -= 250;    // increase top hit area
+                                   rect.left -= 250;   // increase left hit area
+                                   rect.bottom += 250; // increase bottom hit area
+                                   rect.right += 250;  // increase right hit area
+                                   parentReturn.setTouchDelegate(new TouchDelegate(rect, buttonToggle));
+                               }
+                           });
+
+        description.setAnimationDuration(450L);
+
+
+
+         // or set them separately
+        description.setExpandInterpolator(new OvershootInterpolator(1.0f));
+        description.setCollapseInterpolator(new LinearOutSlowInInterpolator());
+
         final Review items = getItem(position);
 
         image.setImageResource(items.getDrawable());
         textView.setText(items.getName());
         description.setText(items.getDescription());
+
+        if (items.getDescription().toString().length() < 100){
+            buttonToggle.setEnabled(false);
+            buttonToggle.setVisibility(View.INVISIBLE);
+        }
+
 
         int vote = items.getVote();
 
@@ -104,84 +130,41 @@ public class CustomListViewAdapter extends ArrayAdapter<Review> {
 
         }
 
-        makeTextViewResizable(description, 3, "See More", true);
-        return convertView;
-    }
-
-    public static void makeTextViewResizable(final TextView tv, final int maxLine, final String expandText, final boolean viewMore) {
-
-        if (tv.getTag() == null) {
-            tv.setTag(tv.getText());
-        }
-        ViewTreeObserver vto = tv.getViewTreeObserver();
-        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-
-            @SuppressWarnings("deprecation")
+        // but, you can also do the checks yourself
+        buttonToggle.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public boolean onPreDraw() {
-
-                Context context;
-
-                ViewTreeObserver obs = tv.getViewTreeObserver();
-                obs.removeOnPreDrawListener(this);
-                if (maxLine == 0) {
-                    int lineEndIndex = tv.getLayout().getLineEnd(0);
-                    String text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
-                    tv.setText(text);
-                    tv.setMovementMethod(LinkMovementMethod.getInstance());
-                    tv.setText(
-                            addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, maxLine, expandText,
-                                    viewMore), TextView.BufferType.SPANNABLE);
-                } else if (maxLine > 0 && tv.getLineCount() >= maxLine) {
-                    int lineEndIndex = tv.getLayout().getLineEnd(maxLine - 1);
-                    String text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
-                    tv.setText(text);
-                    tv.setMovementMethod(LinkMovementMethod.getInstance());
-                    tv.setText(
-                            addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, maxLine, expandText,
-                                    viewMore), TextView.BufferType.SPANNABLE);
-                } else {
-                    int lineEndIndex = tv.getLayout().getLineEnd(tv.getLayout().getLineCount() - 1);
-                    String text = tv.getText().subSequence(0, lineEndIndex) + " " + expandText;
-                    tv.setText(text);
-                    tv.setMovementMethod(LinkMovementMethod.getInstance());
-                    tv.setText(
-                            addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, lineEndIndex, expandText,
-                                    viewMore), TextView.BufferType.SPANNABLE);
+            public void onClick(final View v)
+            {
+                if (description.isExpanded())
+                {
+                    description.collapse();
+                    buttonToggle.setText("Read all");
                 }
-                return true;
+                else
+                {
+                    description.expand();
+                    buttonToggle.setText("Read less");
+                }
             }
         });
 
-    }
+        // listen for expand / collapse events
+        description.setOnExpandListener(new ExpandableTextView.OnExpandListener()
+        {
+            @Override
+            public void onExpand(final ExpandableTextView view)
+            {
+                Log.d("EXPAND", "ExpandableTextView expanded");
+            }
 
-    private static SpannableStringBuilder addClickablePartTextViewResizable(final Spanned strSpanned, final TextView tv,
-                                                                            final int maxLine, final String spanableText, final boolean viewMore) {
-        String str = strSpanned.toString();
-        SpannableStringBuilder ssb = new SpannableStringBuilder(strSpanned);
+            @Override
+            public void onCollapse(final ExpandableTextView view)
+            {
+                Log.d("COLLAPSE", "ExpandableTextView collapsed");
+            }
+        });
 
-        if (str.contains(spanableText)) {
-
-
-            ssb.setSpan(new MySpannable(false){
-                @Override
-                public void onClick(View widget) {
-                    if (viewMore) {
-                        tv.setLayoutParams(tv.getLayoutParams());
-                        tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
-                        tv.invalidate();
-                        makeTextViewResizable(tv, -1, "See Less", false);
-                    } else {
-                        tv.setLayoutParams(tv.getLayoutParams());
-                        tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
-                        tv.invalidate();
-                        makeTextViewResizable(tv, 3, ".. See More", true);
-                    }
-                }
-            }, str.indexOf(spanableText), str.indexOf(spanableText) + spanableText.length(), 0);
-
-        }
-        return ssb;
-
+        return convertView;
     }
 }
