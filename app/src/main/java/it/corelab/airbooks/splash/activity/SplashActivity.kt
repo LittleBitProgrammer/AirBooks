2261baba5e4f9d4bfb668f1b5f9677524d7b1202
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2018-07-14 12:34:23 +0200

    Splash screen restyle
    -restyle splash screen airbooks

--- app/src/main/java/it/corelab/airbooks/splash/activity/SplashActivity.kt
+++ app/src/main/java/it/corelab/airbooks/splash/activity/SplashActivity.kt
@@ -1,19 +1,12 @@
 package it.corelab.airbooks.splash.activity
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.PersistableBundle
-import android.util.AttributeSet
+import android.support.v4.view.ViewCompat
 import android.util.Log
 import android.view.View
-import android.view.animation.OvershootInterpolator
 import android.widget.ImageView
 
 import java.util.Locale
@@ -31,58 +24,46 @@ import it.corelab.airbooks.data.model.remote.APIService
 import it.corelab.airbooks.data.model.remote.ApiUtils
 import it.corelab.airbooks.splash.layout.SplashScreenLayout
 import org.jetbrains.anko.setContentView
+import android.support.v4.view.ViewCompat.animate
+import android.support.v4.view.ViewPropertyAnimatorCompat
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import java.lang.reflect.Array
+
 
 class SplashActivity : AppCompatActivity() {
+    val STARTUP_DELAY = 300
+    val ANIM_ITEM_DURATION = 1000
+    val ITEM_DELAY = 300
+
+    private val animationStarted = false
+
     private var mAPIService: APIService? = null
     private lateinit var centralLogo: ImageView
+    private lateinit var container: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
+
         SplashScreenLayout().setContentView(this)
 
+    }
 
-        val handler = Handler()
-        handler.postDelayed({
-            mAPIService = ApiUtils.getAPIService()
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
 
-            val isFirstRun = getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).getBoolean("isFirstRun", true)
+        if (!hasFocus || animationStarted) {
+            return
+        }
 
-            Log.i("SPLASH AVVIATO", "TRUE")
+        animate()
 
-            if (isFirstRun) {
-                //show start activity
-
-                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
-                overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
-                Log.i("PRIMA RUN", "TRUE")
-
-
-            } else {
-
-                val sharedPreferences = this.getSharedPreferences(this.packageName, android.content.Context.MODE_PRIVATE)
-                val token = sharedPreferences.getString("token", "")
-
-                Log.i("PRIMA RUN", "FALSE")
-
-                if (token!!.equals("", ignoreCase = true)) {
-                    // Show a log in dialog and/or other stuff to let user log in
-                    Log.i("TOKEN VUOTO", "TRUE")
-                    doIntentToLogin()
-                } else {
-                    Log.i("TOKEN VUOTO", "FALSE")
-                    automaticSignInPost("http://airbooks.altervista.org/API/v2/auth/", Locale.getDefault().language, "android", token)
-                    Log.i("TOKEN", token)
-                }
-            }
-
-            getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).edit().putBoolean("isFirstRun", false).apply()
-        }, 1000) // 1000ms delay
-
+        super.onWindowFocusChanged(hasFocus)
     }
 
 
     fun doIntentToLogin() {
-        centralLogo = findViewById(R.id.CENTRAL_LOGO)
         val loginIntent = Intent(this@SplashActivity, Login::class.java)
         loginIntent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
         startActivity(loginIntent)
@@ -116,5 +97,67 @@ class SplashActivity : AppCompatActivity() {
 
             }
         })
+    }
+
+    fun animate(){
+        centralLogo = findViewById(R.id.CENTRAL_LOGO)
+        container = findViewById(R.id.LINEAR_INTERNAL_SPLASH_SCREEN)
+
+        val elements = intArrayOf(0,1)
+
+        ViewCompat.animate(centralLogo)
+                .translationY(-250F)
+                .setStartDelay(STARTUP_DELAY.toLong())
+                .setDuration(ANIM_ITEM_DURATION.toLong())
+                .setInterpolator(DecelerateInterpolator(1.2F))
+                .start()
+
+        for (element: Int in elements){
+             val v: View = container.getChildAt(element)
+             var viewAnimator: ViewPropertyAnimatorCompat
+
+             viewAnimator = ViewCompat.animate(v)
+                    .translationY(50F)
+                    .alpha(1F)
+                    .setStartDelay((STARTUP_DELAY * element).toLong() + 500)
+                    .setDuration(1000)
+
+            viewAnimator.setInterpolator(DecelerateInterpolator())
+                    .withEndAction { mAPIService = ApiUtils.getAPIService()
+
+                        val isFirstRun = getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).getBoolean("isFirstRun", true)
+
+                        Log.i("SPLASH AVVIATO", "TRUE")
+
+                        if (isFirstRun) {
+                            //show start activity
+
+                            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                            overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+                            Log.i("PRIMA RUN", "TRUE")
+
+
+                        } else {
+
+                            val sharedPreferences = this.getSharedPreferences(this.packageName, android.content.Context.MODE_PRIVATE)
+                            val token = sharedPreferences.getString("token", "")
+
+                            Log.i("PRIMA RUN", "FALSE")
+
+                            if (token!!.equals("", ignoreCase = true)) {
+                                // Show a log in dialog and/or other stuff to let user log in
+                                Log.i("TOKEN VUOTO", "TRUE")
+                                doIntentToLogin()
+                            } else {
+                                Log.i("TOKEN VUOTO", "FALSE")
+                                automaticSignInPost("http://airbooks.altervista.org/API/v2/auth/", Locale.getDefault().language, "android", token)
+                                Log.i("TOKEN", token)
+                            }
+                        }
+
+                        getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).edit().putBoolean("isFirstRun", false).apply() }
+                    .start()
+
+        }
     }
 }
--- app/src/main/java/it/corelab/airbooks/splash/layout/SplashScreenLayout.kt
+++ app/src/main/java/it/corelab/airbooks/splash/layout/SplashScreenLayout.kt
@@ -1,6 +1,11 @@
 package it.corelab.airbooks.splash.layout
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.support.constraint.ConstraintLayout.LayoutParams.PARENT_ID
+import android.support.v4.content.res.ResourcesCompat
+import android.view.Gravity
+import android.widget.LinearLayout
 import it.corelab.airbooks.R
 import it.corelab.airbooks.splash.activity.SplashActivity
 import org.jetbrains.anko.*
@@ -10,40 +15,64 @@ class SplashScreenLayout : AnkoComponent<SplashActivity> {
     override fun createView(ui: AnkoContext<SplashActivity>)= with(ui) {
 
         constraintLayout {
-            id = Ids.CONSTRAINT_LAYOUT
+            id = Ids.LINEAR_LAYOUT_EXTERNAL
             this.setBackgroundColor(resources.getColor(R.color.white))
 
+            linearLayout {
+                id = Ids.LINEAR_INTERNAL
+
+                orientation = LinearLayout.VERTICAL
+                gravity = Gravity.CENTER
+
+                textView("Airbooks"){
+                    id = Ids.APP_NAME
+
+                    typeface = ResourcesCompat.getFont(ctx,R.font.koliko)
+                    textSize = sp(15).toFloat()
+                    textColor = resources.getColor(R.color.title_color_splash_screen)
+                    alpha = 0F
+                }.lparams(width = wrapContent, height = wrapContent){
+                    topMargin = dip(16)
+                }
+
+                textView("Leggi, crea, ama"){
+                    id = Ids.SUBTITLE
+
+                    typeface = ResourcesCompat.getFont(ctx,R.font.cookie)
+                    textSize = sp(13).toFloat()
+                    textColor = resources.getColor(R.color.description_color_splash_screen)
+                    alpha = 0F
+                }.lparams(width = wrapContent, height = dip(60)){
+                    topMargin = dip(0)
+                }
+
+            }.lparams(width = matchParent, height = wrapContent){
+                //topPadding = dip(-130)
+                topToTop = Ids.CENTRAL_LOGO
+                topMargin = dip(50)
+            }
             imageView(R.drawable.icon_splash_screen) {
                 id = Ids.CENTRAL_LOGO
 
             }.lparams(width = dip(150), height = dip(150)){
                 topToTop = PARENT_ID
-                leftToLeft = PARENT_ID
-                rightToRight = PARENT_ID
                 bottomToBottom = PARENT_ID
+                rightToRight = PARENT_ID
+                leftToLeft = PARENT_ID
 
                 topMargin = dip(0)
-                bottomMargin = dip(100)
-                rightMargin = dip(0)
                 leftMargin = dip(0)
-            }
-
-            progressBar {
-                id = Ids.PROGRESS_BAR
-
-            }.lparams(width = dip(60), height = dip(60)){
-                topToBottom = Ids.CENTRAL_LOGO
-                leftToLeft = PARENT_ID
-                rightToRight = PARENT_ID
-
-                topMargin = dip(80)
+                rightMargin = dip(0)
+                bottomMargin = dip(0)
             }
         }
     }
 
         private object Ids {
-            const val CONSTRAINT_LAYOUT = R.id.CONSTRAINT_LAYOUT_SPLASH_SCREEN
+            const val LINEAR_LAYOUT_EXTERNAL = R.id.LINEAR_LAYOUT_EXTERNAL_SPLASH_SCREEN
             const val CENTRAL_LOGO = R.id.CENTRAL_LOGO
-            const val PROGRESS_BAR = R.id.PROGRESS_BAR_SPLASH_SCREEN
+            const val LINEAR_INTERNAL = R.id.LINEAR_INTERNAL_SPLASH_SCREEN
+            const val APP_NAME = R.id.APP_NAME_SPLASH_SCREEN
+            const val SUBTITLE = R.id.SUBTITLE
         }
 }
